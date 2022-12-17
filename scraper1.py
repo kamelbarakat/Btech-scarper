937100086b0b43aabd9b996ee798be7ac53ccd1b
kamelbarakat <barakatk272@gmail.com>
2022-12-17 21:56:03 +0200

    first

---
+++ scraper1.py
@@ -0,0 +1,45 @@
+
+import requests
+from bs4 import BeautifulSoup
+import csv
+#price_air= []
+#air_title=[]
+#specefication=[]
+i=[]
+j=[]
+x=[]
+
+
+result = requests.get("https://btech.com/ar/major-domestic-appliances/air-conditioning.html")
+src = result.content
+
+soup = BeautifulSoup(src,'html.parser')
+#print (soup)
+
+air_title= soup.find_all('h2',{'class':'plpTitle'})
+for x in air_title:
+    print("name is",x.text)
+
+price_air=soup.find_all('span',{'class':'price-wrapper'})
+for j in price_air:
+    print("price is",j.text)
+
+links = []
+mylinks = soup.find_all("a" , {"class":"listingWrapperSection"})
+print(len(mylinks))
+for i in mylinks :
+    links.append(i.get("href"))
+
+for i in links :
+    result = requests.get(i)
+    src = result.content
+    soup = BeautifulSoup(src, 'html.parser')
+    specefication = soup.find_all("tbody" , {"id": "product-attribute-specs-table-tbody"} )
+    for i in specefication:
+        print("specefication",i.text)
+
+
+
+
+
+
